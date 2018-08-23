e05e840639d7e278429b80abf9fad22bc917d001
2ps <pshingavi@gmail.com>
2018-08-23 13:22:43 -0500

    [OPS]  Added in base ubuntu install!

--- ubuntu/build_python.sh
+++ ubuntu/build_python.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 function build_python {
     version="$1"
+    echo "building python $version"
     mkdir -p ${work_dir}/downloads
     cd ${work_dir}/downloads
     filename="${work_dir}/downloads/python-$version.tgz"
     if [[ ! -f "$filename" ]] ; then
-        wget "https://www.python.org/ftp/python/${version}/Python-${version}.tgz" -O "$filename" ;
+        wget --quiet "https://www.python.org/ftp/python/${version}/Python-${version}.tgz" -O "$filename" ;
     fi
     pdir="${work_dir}/python-$version"
     if [ ! -d "$pdir" ] ; then
@@ -14,8 +15,11 @@ function build_python {
         tar xzf "${work_dir}/downloads/python-$version.tgz" --strip-components=1
     fi
     cd "$pdir"
-    ./configure && make && make altinstall
+    ./configure >/dev/null 2>&1
+    make >/dev/null 2>&1
+    make altinstall >/dev/null 2>&1
     cd ..
     rm -f "$filename"
     rm -rf "$pdir"
+    echo "done"
 }
