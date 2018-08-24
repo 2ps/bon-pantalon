045046d971c5e1e0f593cbf1757313671b89d5a7
2ps <pshingavi@gmail.com>
2018-08-23 23:47:41 -0500

    [OPS]  Added in build commands for posh

---
+++ build.ps1
@@ -0,0 +1,31 @@
+param(
+    [string]$image
+)
+if (!(Test-Path "${image}")) {
+    Write-Error "no such container image"
+    exit;
+}
+$name = Get-Content ${image}/NAME
+$version = Get-Content ${image}/VERSION
+Write-Host "building ${name}:${version}"
+if (Test-Path "${x}/prebuild.sh") {
+    & bash.exe -c "cd ${image} && ${x}/prebuild.sh ${x}";
+}
+else {
+    if (Test-Path "${x}\prebuild.ps1") {
+        & "${x}/prebuild.ps1"
+    }
+}
+
+# $pwd = (Get-Location).Path
+# Set-Location .\$image
+docker.exe build --build-arg name="${name}" --build-arg version="${version}" --tag "${name}:latest" --tag "${name}:${version}" "${image}"
+# Set-Location $pwd
+if (Test-Path "${x}/postbuild.sh") {
+    & bash.exe -c "cd ${image} && ${x}/postbuild.sh ${x}";
+}
+else {
+    if (Test-Path "${x}\postbuild.ps1") {
+        & "${x}/postbuild.ps1"
+    }
+}
